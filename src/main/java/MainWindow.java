fc5ee0ae1522a0045f67c530d6dc2aec755a848b
RedkinMichail <redkin.michail.a@gmail.com>
2017-10-14 18:18:22 +0300

    Создано пустое окно JavaFX

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/MainWindow.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("MainWindow");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
